0378eed50e076595573c8a06085b4e9f61163ff3
Mahmoud Gamal <mahmoudgamalawad10@gmail.com>
2022-04-01 03:15:01 +0200

    Header icon feature

---
+++ action.js
@@ -0,0 +1,15 @@
+// Rotating the icon and showing the links once the icon is clicked and hiding the links when clicked outside.
+let headerIcon = document.getElementsByTagName("i")[0];
+let headerDropDown = document.getElementsByTagName("ul")[0];
+
+document.addEventListener("click", function (e) {
+  if (e.target === headerIcon) {
+    headerIcon.style.transform = "rotate(-180deg)";
+    headerDropDown.style.display = "block";
+  }
+
+  if (e.target !== headerIcon) {
+    headerIcon.style.transform = "rotate(-90deg)";
+    headerDropDown.style.display = "none";
+  }
+});
