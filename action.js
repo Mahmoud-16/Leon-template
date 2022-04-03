9d06c51ad958df9b582f64b566072b0a40dc4b55
Mahmoud Gamal <mahmoudgamalawad10@gmail.com>
2022-04-03 04:20:23 +0200

    Features Section

--- action.js
+++ action.js
@@ -1,4 +1,4 @@
-// Rotating the icon and showing the links once the icon is clicked and hiding the links when clicked outside.
+// Rotating the header icon and showing the links once the icon is clicked and hiding the links when clicked outside.
 let headerIcon = document.getElementsByTagName("i")[0];
 let headerDropDown = document.getElementsByTagName("ul")[0];
 
@@ -6,9 +6,7 @@ document.addEventListener("click", function (e) {
   if (e.target === headerIcon) {
     headerIcon.style.transform = "rotate(-180deg)";
     headerDropDown.style.display = "block";
-  }
-
-  if (e.target !== headerIcon) {
+  } else {
     headerIcon.style.transform = "rotate(-90deg)";
     headerDropDown.style.display = "none";
   }
